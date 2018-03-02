442e59b515a309a9bdb78504578770c67709c81f
Bartek <bartekma89@gmail.com>
2018-03-02 19:21:01 +0100

    added mixins to helpers

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browser-sync', ['sass'], function() {
 	browserSync.init({
@@ -16,6 +17,9 @@ gulp.task('browser-sync', ['sass'], function() {
 gulp.task('sass', function() {
 	return gulp.src('src/scss/**/*.scss')
 	.pipe(sass().on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 3 versions']
+	}))
 	.pipe(gulp.dest('src/css'))
 	.pipe(browserSync.stream())
 })
